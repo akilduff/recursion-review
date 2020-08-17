1d3ab538ce823224e36cdf514483a30b00937c7b
Owen Wexler <owenbwexler@gmail.com>
2020-08-17 18:13:54 -0400

    Begin implmenting getElementsByClassName and fix tests

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,5 +6,34 @@
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className
 ) {
-  // your code here
+  // declare result
+  var result = [];
+
+  var traverseDOM = function(node, callback) {
+    while (node) {
+      var nodeList = node.childNodes;
+      if (nodeList[0]) {
+        callback(nodeList[0]);
+        traverseDOM();
+      }
+    }
+    // if the node has children
+    // recurse through the node's children
+    //otherwise run the callback on the node and return
+    //callback:
+    // if the nodeList is empty, check if the className is what we want
+    //node.classList[0] === "className"
+    // if it IS what we want, push it to our result array
+
+
+  };
+  //DOM traversal helper function
+  // input: node, callback
+  // output: a node that has no children, and has the specified class name
+
+  traveseDOM(element, function () {
+    // callback function here
+  });
+
+  return result;
 };
