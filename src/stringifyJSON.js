66a823062cf2f66edf3838e00193d3816e10ea03
Owen Wexler <owenbwexler@gmail.com>
2020-08-17 14:22:46 -0400

    add pseudocode for stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,19 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  // your code goes here askdfjhaslkdjgheriughaSDHJHJHJAHSUH
+
+  // declare result
+  // check type of object
+  // number - return stringified number
+  // string - return string
+  // boolean - return stringified boolean
+  // null - the word 'null' stringified
+  // undefined - undefined?
+
+  // arrays - '[' + recurse through array and keep returning result for each index + ']';
+  // objects - '{' + recurse through object and keep returning result for each index '}']
+
+
 };
+
